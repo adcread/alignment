4dd4c819e888be78c2c5e044b63d2404ef6d9ba4
unknown <cw13377@IT055654.users.bris.ac.uk>
2015-09-03 16:47:36 +0100

    uyvgubub

--- Equalisation/inversionEqualiser.m
+++ Equalisation/inversionEqualiser.m
@@ -1,30 +1,52 @@
 
 % Performs the channel inversion equalisation from [66] Weikert
 
-addpath('/Users/chris/PhD/alignment/Channel/');
-addpath('/Users/chris/PhD/alignment/General functions/');
+if ispc
+    addpath('C:\PhD\alignment\Channel\');
+    addpath('C:\PhD\alignment\General functions\');
+elseif ismac
+    addpath('/Users/chris/PhD/alignment/Channel/');
+    addpath('/Users/chris/PhD/alignment/General functions/');
+end
 
-H = generateChannel(1,3,2,'kronecker');
-H = H{1,1}
+txAntennas = [3 2];
+rxAntennas = [3 2];
+
+H = generateChannel(2,rxAntennas,txAntennas,'kronecker');     % create a 2-user MIMO network with M=[3 2], N=[3 2].
+
+%celldisp(H);
+
+for i = 1:2
+    for j = 1:2
+        
+        H_r{i,j} = H{j,i}';
+        
+    end
+end
+
+for i = 1:2
+    for j = 1:2
+        
+        [U{i,j}, sigma{i,j},V{i,j}] = svd(H{i,j});
+        [U_r{i,j}, sigma_r{i,j}, V_r{i,j}] = svd(H_r{i,j});
+        
+    end
+end
 
 SNR = 500;
 
-sequenceLength = 64;
-
-sequence = circSymAWGN(3,sequenceLength,1);
+sequenceLength = 10240;
 
 for i = 1:2
     
-    receivedSignal(i,:) =  sqrt(SNR) * H(i,:) * sequence;% + circSymAWGN(1,sequenceLength,1);
-        
+    sequence{i} = circSymAWGN(txAntennas(i),sequenceLength,1);
+    
 end
 
-for i = 1:2
+receivedSignal{1} = sqrt(SNR) * eye(3)' * H{2,1} * V{2,1} * sequence{2};
 
-    H_est(i,:) =  1/sqrt(SNR) * receivedSignal(i,:) * (pinv(sequence(1:2,:)'*sequence(1:2,:)) * sequence(1:2,:)');
-   
-end
+receivedSignal{2} = sqrt(SNR) * eye(2)' * H{1,2} * V{1,2} * sequence{1};
 
-H_est
+[U_est{2,1}, sigma_est{2,1}] = sortEigs(receivedSignal{1}*receivedSignal{1}');
 
-err = H - H_est
+[U_est{1,2}, sigma_est{1,2}] = sortEigs(receivedSignal{2}*receivedSignal{2}');
---
+++ Equalisation/reciprocalEqualiser.m
@@ -0,0 +1,81 @@
+% 'Uses a combination of SVD and EVD to find the precoding matrices and
+% ranks of each user in a 2-user MIMO interference channel with unequal
+% numbers of Tx and Rx antennas.'
+
+if ispc
+    addpath('C:\PhD\alignment\Channel\');
+    addpath('C:\PhD\alignment\General functions\');
+elseif ismac
+    addpath('/Users/chris/PhD/alignment/Channel/');
+    addpath('/Users/chris/PhD/alignment/General functions/');
+end
+
+txAntennas = [3 2];
+rxAntennas = [3 2];
+
+H = generateChannel(2,rxAntennas,txAntennas,'kronecker');     % create a 2-user MIMO network with M=[3 2], N=[3 2].
+
+%celldisp(H);
+
+for i = 1:2
+    for j = 1:2
+        
+        H_r{i,j} = H{j,i}';
+        
+    end
+end
+
+for i = 1:2
+    for j = 1:2
+        
+        [Ux{i,j}, sigmax{i,j},Vx{i,j}] = svd(H{i,j});
+        [Ux_r{i,j}, sigmax_r{i,j}, Vx_r{i,j}] = svd(H_r{i,j});
+        
+    end
+end
+
+SNR = [500 100 ; 100 500];  % slight attenuation on the cross links - doesn't really make a difference.
+
+sequenceLength = 10240;
+
+for i = 1:2
+    
+    sequence{i} = circSymAWGN(txAntennas(i),sequenceLength,1);
+    
+end
+
+% message 1 - Transmitter 1 sends rank-3 training sequence
+
+y{1} = sqrt(SNR(1,1)) * H{1,1} * eye(3) * sequence{1};
+
+y{2} = sqrt(SNR(1,2)) * H{1,2} * eye(3) * sequence{1};
+
+[U{1,1}, lambda{1,1}] = sortEigs(y{1}*y{1}');
+
+lambda{1,1} = 1/(SNR(1,1) * sequenceLength) * lambda{1,1};
+
+[U{1,2}, lambda{1,2}] = sortEigs(y{2}*y{2}');
+
+lambda{1,2,1} = 1/(SNR(1,2) * sequenceLength) * lambda{1,2,1};
+
+V_r{2,1} = U{1,2};
+
+% message 2 - Receiver 2 sends rank-2 training sequence
+
+y{1} = sqrt(SNR(2,1)) * H_r{2,1} * V_r{2,1} * sequence{2};
+
+y{2} = sqrt(SNR(2,2)) * H_r{2,2} * V_r{2,1} * sequence{2};
+
+[U{2,1}, lambda{2,1}] = sortEigs(y{1}*y{1}');
+
+lambda{2,1} = 1/(SNR(2,1) * sequenceLength) * lambda{2,1};
+
+[U{2,2}, lambda{2,2}] = sortEigs(y{2}*y{2}');
+
+lambda{2,2,2} = 1/(SNR(2,2) * sequenceLength) * lambda{2,2,2};
+
+sigma{2,1,2} = sqrt(lambda{2,1,2});
+
+x = 1/sqrt(SNR(2,2)) * y{2,2} * pinv(sequence{2});
+
+precoder{1,2,2} = sqrt(postcoder{1,2,1} * pinv(sigma{2,1,2}) * x);
